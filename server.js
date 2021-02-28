ed8dcabb4e1df0e5418559e2de0c46aa7f3814f1
samclarkb <samclarkboot@planet.nl>
2021-02-28 21:49:54 +0100

    edit

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
 var express = require('express');
-// var hbs = require ('express-handlebars');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
 
 // app.set('views', './views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.render("index");
+  res.render("formulier");
 });
 
-app.get('/about', function(req, res){
-  res.send("Hallo about");
+app.post('/resultaten', urlencodedParser, function(req, res){
+  console.log(req.body);
+  res.render("resultaten");
 });
 
-app.get('/login', function(req, res){
-  res.send("Hallo login");
+app.get('/profieleen', function(req, res){
+  res.render('profieleen');
 });
+
+app.get('/profieltwee', function(req, res){
+  res.render('profieltwee');
+});
+
+app.get('/profieldrie', function(req, res){
+  res.render('profieldrie');
+});
+
+app.get('/profielvier', function(req, res){
+  res.render('profielvier');
+});
+
 
 app.get("*", function(req, res) {
   res.render("error")
 });
-
 
 app.listen(3000, function(){
 })
